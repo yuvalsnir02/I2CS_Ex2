206cd636b0fb2b145085bc37ec5010f7ea86603f
yuvalsnir02 <yuvalsnir02@gmail.com>
2025-12-27 19:42:56 +0200

    Create Index2D.java

---
+++ Index2D.java
@@ -0,0 +1,41 @@
+package assignments.Ex2;
+
+public class Index2D implements Pixel2D {
+    public Index2D(int w, int h) {
+        ;
+    }
+    public Index2D(Pixel2D other) {
+        ;
+    }
+    @Override
+    public int getX() {
+
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+
+        return 0;
+    }
+
+    @Override
+    public double distance2D(Pixel2D p2) {
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        String ans = null;
+
+        return ans;
+    }
+
+    @Override
+    public boolean equals(Object p) {
+        boolean ans = true;
+
+        return ans;
+    }
+}
