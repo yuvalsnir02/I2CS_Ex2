ca73aea77b45586469b82b7c1d38a2e6f93808a8
yuvalsnir02 <yuvalsnir02@gmail.com>
2025-12-27 19:45:58 +0200

    Create MapTest.java

---
+++ MapTest.java
@@ -0,0 +1,45 @@
+package assignments.Ex2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Intro2CS, 2026A, this is a very
+ */
+class MapTest {
+    /**
+     */
+    private int[][] _map_3_3 = {{0,1,0}, {1,0,1}, {0,1,0}};
+    private Map2D _m0, _m1, _m3_3;
+    @BeforeEach
+    public void setuo() {
+        _m3_3 = new Map(_map_3_3);
+    }
+    @Test
+    @Timeout(value = 1, unit = SECONDS)
+    void init() {
+        int[][] bigarr = new int [500][500];
+        _m1.init(bigarr);
+        assertEquals(bigarr.length, _m1.getWidth());
+        assertEquals(bigarr[0].length, _m1.getHeight());
+        Pixel2D p1 = new Index2D(3,2);
+        _m1.fill(p1,1, true);
+    }
+
+    @Test
+    void testInit() {
+        _m0.init(_map_3_3);
+        _m1.init(_map_3_3);
+        assertEquals(_m0, _m1);
+    }
+    @Test
+    void testEquals() {
+        assertEquals(_m0,_m1);
+        _m0.init(_map_3_3);
+        _m1.init(_map_3_3);
+        assertEquals(_m0,_m1);
+    }
+}
