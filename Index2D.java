881e3746fae94b1adbe0a3746b3cf142826accab
yuvalsnir02 <yuvalsnir02@gmail.com>
2025-12-28 22:13:38 +0200

    Update Index2D.java

--- Index2D.java
+++ Index2D.java
@@ -1,41 +1,58 @@
 package assignments.Ex2;
 
+import java.util.Objects;
+
 public class Index2D implements Pixel2D {
+    private final int _x;
+    private final int _y;
+
     public Index2D(int w, int h) {
-        ;
+
+        this._x = w;
+        this._y = h;
+
     }
+
     public Index2D(Pixel2D other) {
-        ;
+        if (other == null) {
+            throw new RuntimeException("Index2D copy-ctor: other is null");
+        }
+        this._x = other.getX();
+        this._y = other.getY();
     }
+
     @Override
     public int getX() {
-
-        return 0;
+        return _x;
     }
 
     @Override
     public int getY() {
-
-        return 0;
+        return _y;
     }
 
     @Override
     public double distance2D(Pixel2D p2) {
-
-        return 0;
+        if (p2 == null) throw new RuntimeException("distance2D: p2 is null");
+        int dx = this.x - p2.getX();
+        int dy = this.y - p2.getY();
+        return Math.sqrt((double) dx * dx + (double) dy * dy );
     }
 
     @Override
     public String toString() {
-        String ans = null;
-
-        return ans;
+        return _x + "," + _y;
     }
 
     @Override
     public boolean equals(Object p) {
-        boolean ans = true;
-
-        return ans;
+        if (this == p) return true;
+        if (p == null) return false;
+        if (p instanceof Pixel2D) {
+            Pixel2D other = (pixel2D) p;
+            return this._x == other.getX() && this._y == other.getY();
+        }
+        return false;
     }
 }
+
